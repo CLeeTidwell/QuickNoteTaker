aaab71fd3f79db31576ebfd0647a7e2e304dbb4a
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-05-26 20:55:44 -0700

    rearranged files and folders

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.use(express.json());
 
 
 // Static Middleware
-app.use(express.static("./public"));
+app.use(express.static("./develop/public"));
 
 
 // API Route | "GET" request
 app.get("/api/notes", function(req, res) {
-  readFileAsync("./db/db.json", "utf8").then(function(data) {
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
       notes = [].concat(JSON.parse(data))
       res.json(notes);
     })
@@ -34,13 +34,13 @@ app.get("/api/notes", function(req, res) {
 // API Route | "POST" request
 app.post("/api/notes", function(req, res) {
     const note = req.body;
-    readFileAsync(".db/db.json", "utf8").then(function(data) {
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
       const notes = [].concat(JSON.parse(data));
       note.id = notes.length + 1
       notes.push(note);
       return notes
     }).then(function(notes) {
-      writeFileAsync("./db/db.json", JSON.stringify(notes))
+      writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
       res.json(note);
     })
 });
@@ -49,7 +49,7 @@ app.post("/api/notes", function(req, res) {
 // API Route | "DELETE" request
 app.delete("/api/notes/:id", function(req, res) {
   const idToDelete = parseInt(req.params.id);
-  readFileAsync(".db/db.json", "utf8").then(function(data) {
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
     const notes = [].concat(JSON.parse(data));
     const newNotesData = []
     for (let i = 0; i<notes.length; i++) {
@@ -59,7 +59,7 @@ app.delete("/api/notes/:id", function(req, res) {
     }
     return newNotesData
   }).then(function(notes) {
-    writeFileAsync(".db/db.json", JSON.stringify(notes))
+    writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
     res.send('saved success!!!');
   })
 })
@@ -67,15 +67,15 @@ app.delete("/api/notes/:id", function(req, res) {
 
 // HTML Routes
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, ".public/notes.html"));
+  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
   });
 
 app.get("/", function(req, res) {
-     res.sendFile(path.join(__dirname, "./public/index.html"));
+     res.sendFile(path.join(__dirname, "./develop/public/index.html"));
   });
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
  });
 
 
